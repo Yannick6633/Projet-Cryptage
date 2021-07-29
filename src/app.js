56561e71d3b07de3433ecfcc144e549b8652586e
Evan-Rousseau <61151018+Evan-Rousseau@users.noreply.github.com>
2021-07-29 10:49:55 +0200

    Add: Function crypting and decryping

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 function crypting(){
-    console.log('triggered');
+    let message = document.getElementById('message').value;
+    let passphrase = document.getElementById('cle').value;
+    if((message != '') && (passphrase != '')){
+        let encryptedAES = CryptoJS.AES.encrypt(message, passphrase);
+        document.getElementById('result').innerHTML = encryptedAES;
+    }
 }
 
 function decrypting(){
-    console.log('untriggered');
+    let message = document.getElementById('message').value;
+    let passphrase = document.getElementById('cle').value;
+    if((message != '') && (passphrase != '')){
+        let decryptedBytes = CryptoJS.AES.decrypt(message, passphrase);
+        let plaintext = decryptedBytes.toString(CryptoJS.enc.Utf8);
+        document.getElementById('result').innerHTML = plaintext;
+
+    }
 }
